05d156139d8f36e537e418e35109f1926e16a0c3
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-06 14:36:04 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.new(post_params)
+    @post.category = params.permit(:category)
     if @post.valid?
       @post.update(post_params)
       redirect_to post_path(@post)
